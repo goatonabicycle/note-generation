a4ef419d87e240e4b7a52b0a9da956bc9de1bdc9
oldtimerza <bradleymorrisza@gmail.com>
2020-09-27 17:32:10 +0200

    Add first test and refactored patterns for injection of dependencies

--- src/client/public/js/patterns.js
+++ src/client/public/js/patterns.js
@@ -1,10 +1,16 @@
-function patterns(noteObjects, tempo, updateUIFromState) {
+function patterns(Tone, noteObjects, tempo, updateUIFromState) {
   this.currentItem = 0;
   this.timer = 0;
   this.tempo = tempo;
   this.mainMelody = [];
   this.playing = false;
   this.updateUIFromState = updateUIFromState;
+  this.synth = Tone.Synth({
+    oscillator: {
+      count: 4,
+      // type: "fatsawtooth",
+    },
+  }).toDestination();
 
   noteObjects.forEach((element, index) => {
     this.mainMelody.push({
@@ -13,70 +19,62 @@ function patterns(noteObjects, tempo, updateUIFromState) {
       noteObject: element,
     });
   });
-}
 
-patterns.prototype.synth = new Tone.Synth({
-  oscillator: {
-    count: 4,
-    // type: "fatsawtooth",
-  },
-}).toDestination();
+  this.loopThroughNotes = () => {
+    const currentNote = this.mainMelody[this.currentItem];
 
+    //play the sound for this item
+    this.synth.triggerAttackRelease(
+      currentNote.note,
+      currentNote.duration,
+      currentNote.time
+    );
 
-patterns.prototype.loopThroughNotes = function () {
-  const currentNote = this.mainMelody[this.currentItem];
+    //update UI
+    this.updateUIFromState({
+      currentItem: this.currentItem,
+      tempo: this.tempo
+    });
 
-  //play the sound for this item
-  this.synth.triggerAttackRelease(
-    currentNote.note,
-    currentNote.duration,
-    currentNote.time
-  );
+    //Next item
+    this.currentItem++;
 
-  //update UI
-  this.updateUIFromState({
-    currentItem: this.currentItem,
-    tempo: this.tempo
-  });
+    if (this.currentItem === this.mainMelody.length) this.currentItem = 0;
+  };
 
-  //Next item
-  this.currentItem++;
-
-  if (this.currentItem === this.mainMelody.length) this.currentItem = 0;
-};
-
-patterns.prototype.play = async function () {
-  await Tone.start();
-  if (this.playing) {
-    clearInterval(this.timer);
-    this.playing = false;
+  this.play = async () => {
+    await Tone.start();
+    if (this.playing) {
+      clearInterval(this.timer);
+      this.playing = false;
+      return this.playing;
+    }
+    this.loopThroughNotes();
+    const tempo = 60000 / this.tempo;
+    this.timer = setInterval(this.loopThroughNotes.bind(this), tempo); //applyToAllNotes gets executed from Window context, bind this so that we have to instance properties.
+    this.playing = true;
     return this.playing;
-  }
-  this.loopThroughNotes();
-  const tempo = 60000 / this.tempo;
-  this.timer = setInterval(this.loopThroughNotes.bind(this), tempo); //applyToAllNotes gets executed from Window context, bind this so that we have to instance properties.
-  this.playing = true;
-  return this.playing;
-};
+  };
 
-patterns.prototype.updateTempo = function (tempo) {
-  this.tempo = tempo;
-  if (this.playing) {
-    this.loopWithNewTempo();
+  this.updateTempo = (tempo) => {
+    this.tempo = tempo;
+    if (this.playing) {
+      this.loopWithNewTempo();
+    }
+
+    //update UI
+    this.updateUIFromState({
+      currentItem: this.currentItem,
+      tempo: this.tempo
+    });
   }
 
-  //update UI
-  this.updateUIFromState({
-    currentItem: this.currentItem,
-    tempo: this.tempo
-  });
+  this.loopWithNewTempo = () => {
+    clearInterval(this.timer);
+    const tempo = 60000 / this.tempo;
+    this.timer = setInterval(this.loopThroughNotes.bind(this), tempo); //loopThroughNotes gets executed from Window context, bind this so that we have to instance properties.
+  }
 }
-
-patterns.prototype.loopWithNewTempo = function () {
-  clearInterval(this.timer);
-  const tempo = 60000 / this.tempo;
-  this.timer = setInterval(this.loopThroughNotes.bind(this), tempo); //loopThroughNotes gets executed from Window context, bind this so that we have to instance properties.
-};
 
 export {
   patterns
--- src/client/public/js/ui.js
+++ src/client/public/js/ui.js
@@ -1,101 +1,69 @@
 import {
-  setUrl,
-  createSharableUrl,
-  removeUrlParameter,
-  copyStringToClipboard
+    setUrl
 } from "./utils.js";
 import {
-  patterns
-} from "./patterns.js";
-
-const defaultTempo = "240"
+    patterns,
+} from "./patterns.js"
 
 const playButton = document.getElementById("play-button");
 const shareButton = document.getElementById("note-share-button");
 const refreshButton = document.getElementById("refresh-button");
 const tempoSlider = document.getElementById("tempoRange");
-const selectedTempo = document.getElementById("tempo") || defaultTempo;
+const selectedTempo = document.getElementById("tempo") || "240";
 const selectedNumberOfNotes = document.getElementById("notes");
 const selectedKey = document.getElementById("key");
 const selectedScale = document.getElementById("scale");
 const noteObjects = document.querySelectorAll(".note-item");
 const currentPattern = document.getElementById("current-pattern").innerText;
 
-const sharePanel = document.getElementById("share-url");
-
 function updateUI(state) {
-  //Set current playing note with color
-  noteObjects[noteObjects.length - 1].classList.remove("playing");
+    //Set current playing note with color
+    noteObjects[noteObjects.length - 1].classList.remove("playing");
 
-  noteObjects[state.currentItem].classList.add("playing");
+    noteObjects[state.currentItem].classList.add("playing");
 
-  if (state.currentItem > 0) {
-    noteObjects[state.currentItem - 1].classList.remove("playing");
-  }
-}
+    if (state.currentItem > 0) {
+        noteObjects[state.currentItem - 1].classList.remove("playing");
+    }
+};
 
-var patternsInstance = new patterns(noteObjects, tempoSlider.value, updateUI);
+const _patterns = new patterns(Tone, noteObjects, tempoSlider.value, updateUI);
 
 playButton.addEventListener("click", async () => {
-  const playing = await patternsInstance.play();
-  playButton.innerHTML = playing ? "Stop" : "Play";
+    const playing = await _patterns.play();
+    playButton.innerHTML = playing ? "Stop" : "Play";
 });
 
-
-
 refreshButton.addEventListener("click", async () => {
-  document.location = removeUrlParameter(document.location + '', "pattern");
+    document.location.reload(true);
 });
 
 shareButton.addEventListener("click", async () => {
-  function shareParameter(name, value) {
-    this.name = name;
-    this.value = value;
-  }
-
-  const shareParameterArray = [];
-  shareParameterArray.push(new shareParameter("key", selectedKey.value));
-  shareParameterArray.push(new shareParameter("scale", selectedScale.value));
-
-  shareParameterArray.push(
-    new shareParameter("notes", selectedNumberOfNotes.value)
-  );
-  shareParameterArray.push(
-    new shareParameter("tempo", tempoSlider.value)
-  );
-  shareParameterArray.push(
-    new shareParameter("pattern", currentPattern)
-  );
-
-  const shareableUrl = createSharableUrl(shareParameterArray);
-  sharePanel.innerText = shareableUrl;
-
-
-  const copiedNotification = document.createElement("div");
-  const copiedTextElement = document.createTextNode("Copied to your clipboard!");
-  copiedNotification.appendChild(copiedTextElement);
-  sharePanel.append(copiedNotification);
-
-  copyStringToClipboard(shareableUrl)
-  console.log("shareableUrl", shareableUrl);
+    //todo: Build the url with all selected items and current set of random notes here.
+    alert(`Coming soon! But your pattern is: ${currentPattern} `);
 });
 
 tempoSlider.onchange = () => {
-  const tempo = tempoSlider.value;
-  patternsInstance.updateTempo(tempo);
-  selectedTempo.innerHTML = tempo;
-  window.localStorage.setItem("tempo", tempo);
+    const tempo = tempoSlider.value;
+    _patterns.updateTempo(tempo);
+    selectedTempo.innerHTML = tempo;
+    window.localStorage.setItem("tempo", tempo);
 };
 
 const setUrlQueryParam = (key) => (sender) => {
-  setUrl(key, sender.target);
-};
+    setUrl(key, sender.target);
+}
 
 selectedNumberOfNotes.onchange = setUrlQueryParam("notes");
 selectedKey.onchange = setUrlQueryParam("key");
 selectedScale.onchange = setUrlQueryParam("scale");
 
-const initialTempo = window.localStorage.getItem("tempo") || defaultTempo;
+var href = new URL(window.location.href);
+selectedNumberOfNotes.value = href.searchParams.get("notes") || 8;
+selectedScale.value = href.searchParams.get("scale") || "lydian";
+selectedKey.value = href.searchParams.get("key") || "C";
+
+const initialTempo = window.localStorage.getItem("tempo") || "240"
 tempoSlider.value = initialTempo;
 selectedTempo.innerHTML = initialTempo;
-patternsInstance.updateTempo(initialTempo);
+_patterns.updateTempo(initialTempo);
